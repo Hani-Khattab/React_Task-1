dd08b571b4af5d6eeb7dfe0860025b0bd1294d78
Hani-Khattab <hanikhattab266@gmail.com>
2026-01-23 17:26:42 +0200

    About Section

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,1 +1,36 @@
+import React from 'react'
+import style from './About.module.css';
+import { FaStar } from 'react-icons/fa';
 
+
+export default function AboutSection() {
+  return (
+    <section className={style.AboutSection}>
+        <div className="porfolioTitle">
+                            <h1 className={` mt-4 fw-bold text-white text-center ${style.porfolioTitle} `} > ABOUT</h1>
+                            <div className={`${style.divider} d-flex align-items-center justify-content-center gap-2 my-4`}>
+                              <span className={style.line}></span>
+                              <FaStar className= {style.FaStar} />
+                              <span className={style.line}></span>
+                </div>
+                </div>
+
+
+                <div className={`${style.aboutInfo} d-flex  justify-content-center`}>
+                <div className="leftside text-center text-white w-25">
+                    <p>Freelancer is a free bootstrap theme created by Start Bootstrap. 
+                    The download includes the complete source files including HTML, CSS, and
+                     JavaScript as well as optional SASS stylesheets for easy customization.</p>
+                </div>
+
+                <div className="rightside text-center text-white w-25">
+                    <p>You can create your own custom avatar for the masthead, change the icon in the dividers, and 
+                    add your email address to the contact form to make it fully functional!</p>
+                </div>
+            </div>
+            <div className='d-flex justify-content-center mt-5 '>
+             <button className={`${style.aboutBtn}`}>Free Download! </button>
+             </div>
+    </section>
+  )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
 import Portfolio from "./Portfolio/Portfolio";
-
+import About from "./About/About";
 function App(){
 
 return <>
 <Navbar />
 <Hero />
 <Portfolio />
-
+<About />
 
 
 </>
